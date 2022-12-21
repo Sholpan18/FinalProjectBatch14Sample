c662223648dcddd15a041a93953bd5883022e4d1
sholpan <sholpan1889@gmail.com>
2022-12-20 20:11:39 -0500

    I have added feature planet2 and it is working fine

--- src/FeatureSun.java
+++ src/FeatureSun.java
@@ -1,7 +1,6 @@
 public class FeatureSun {
-    public static void main(String[] args) {
+
         String color;
         String temp;
         String size;
     }
-}
--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,11 @@ public class Main {
         Fxron.size="4382742km";
         Fxron.temp="-23478274372k";
 
+        Planet2 Boris=new Planet2();
+        Boris.color="pink";
+        Boris.size=3456;
+        Boris.rings="1";
+        Boris.temperature="7654k";
 
     }
 }
